92028d66770bf274850012a6df7f08b1e6aa0865
sham4sh <joshuashamash+1@gmail.com>
2024-04-10 17:43:01 -0400

    GPS locator first pass

--- Park Patrol/src/main.cpp
+++ Park Patrol/src/main.cpp
@@ -20,7 +20,9 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
-#include <TFLI2C.h>    
+#include <TFLI2C.h>   
+#include <TinyGPSPlus.h> 
+#include <SoftwareSerial.h> 
 
 #define BMP_SCK 13
 #define BMP_MISO 12
@@ -29,6 +31,12 @@
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
+static const int RXPin = 0, TXPin = 1; 
+static const uint32_t GPSBaud = 9600;
+
+TinyGPSPlus gps; 
+SoftwareSerial ss(RXPin, TXPin);
+
 Adafruit_BMP3XX bmp;
 
 TFLI2C tflI2C;
@@ -36,10 +44,38 @@ TFLI2C tflI2C;
 int16_t  tfDist;    // distance in centimeters
 int16_t  tfAddr = TFL_DEF_ADR;  // Use this default I2C address
 
+void displayInfo() { 
+// Displaying Google Maps link with latitude and longitude information if GPS location is valid 
+
+if (gps.location.isValid()) { 
+
+Serial.print("http://www.google.com/maps/place/"); 
+Serial.print(gps.location.lat(), 6); 
+Serial.print(F(",")); 
+Serial.println(gps.location.lng(), 6); } else { Serial.print(F("INVALID"));
+} // Displaying date and time information if valid 
+
+
+
+if (gps.date.isValid()) { 
+
+// Displaying date in MM/DD/YYYY format // If the date is invalid, it prints "INVALID" 
+
+} else 
+
+{ Serial.print(F("INVALID")); } // Displaying time in HH:MM:SS format if valid // If the time is invalid, it prints "INVALID" 
+
+}
+
+
 void setup() {
   Wire.begin();
 
   Serial.begin(9600);
+
+  ss.begin(GPSBaud); 
+  Serial.println(F("DeviceExample.ino")); // Printing information for demonstration and version of TinyGPSPlus library // by Mikal Hart 
+
   while (!Serial);
   Serial.println("Adafruit BMP388 / BMP390 test");
 
@@ -87,5 +123,19 @@ void loop() {
     Serial.println("LiDAR read failed");
   }
 
+  
+
+  if (gps.encode(ss.read())) { 
+    Serial.println("GPS found");
+    displayInfo(); 
+  }else{
+    Serial.println("GPS not found");
+  }
+
+  if (millis() > 5000 && gps.charsProcessed() < 10) { 
+    Serial.println(F("No GPS detected: check wiring.")); 
+  while(true); }
+
   delay(200);
 }
+
