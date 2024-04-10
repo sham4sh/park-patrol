6ac7feaaf56f13367b04b2fb317131b5a5f9e0be
sham4sh <joshuashamash+1@gmail.com>
2024-04-10 16:25:06 -0400

    Altitude functional build

--- Park Patrol/src/main.cpp
+++ Park Patrol/src/main.cpp
@@ -1,40 +1,70 @@
-#include <Arduino.h>
+/***************************************************************************
+  This is a library for the BMP3XX temperature & pressure sensor
 
-double thrust;
-double pitch;
-double yaw;
-double roll;
+  Designed specifically to work with the Adafruit BMP388 Breakout
+  ----> http://www.adafruit.com/products/3966
 
-int flport = 0;
-int frport = 0;
-int blport = 0;
-int brport = 0;
+  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
+  to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BMP3XX bmp;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(frport, OUTPUT);
-  pinMode(flport, OUTPUT);
-  pinMode(brport, OUTPUT);
-  pinMode(blport, OUTPUT);
   Serial.begin(9600);
-  
+  while (!Serial);
+  Serial.println("Adafruit BMP388 / BMP390 test");
+
+  if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {   // hardware I2C mode, can pass in address & alt Wire
+  //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
+  //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  double frontRight = thrust + yaw + pitch + roll;
-  double frontLeft = thrust - yaw + pitch - roll;
-  double backRight = thrust - yaw - pitch + roll;
-  double backLeft = thrust + yaw - pitch - roll;
+  if (! bmp.performReading()) {
+    Serial.println("Failed to perform reading :(");
+    return;
+  }
+  Serial.print("Temperature = ");
+  Serial.print(bmp.temperature);
+  Serial.println(" *C");
 
-  analogWrite(frport, frontRight);
-  analogWrite(flport, frontLeft);
-  analogWrite(brport, backRight);
-  analogWrite(blport, backLeft);
-  
-}
+  Serial.print("Pressure = ");
+  Serial.print(bmp.pressure / 100.0);
+  Serial.println(" hPa");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.println();
+  delay(2000);
 }
