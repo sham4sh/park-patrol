6a3234caaf5ed63df86a556dfc38968960caa0b5
sham4sh <joshuashamash+1@gmail.com>
2024-04-10 17:11:20 -0400

    LiDAR functional build

--- Park Patrol/src/main.cpp
+++ Park Patrol/src/main.cpp
@@ -15,10 +15,12 @@
   BSD license, all text above must be included in any redistribution
  ***************************************************************************/
 
+#include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
+#include <TFLI2C.h>    
 
 #define BMP_SCK 13
 #define BMP_MISO 12
@@ -29,7 +31,14 @@
 
 Adafruit_BMP3XX bmp;
 
+TFLI2C tflI2C;
+ 
+int16_t  tfDist;    // distance in centimeters
+int16_t  tfAddr = TFL_DEF_ADR;  // Use this default I2C address
+
 void setup() {
+  Wire.begin();
+
   Serial.begin(9600);
   while (!Serial);
   Serial.println("Adafruit BMP388 / BMP390 test");
@@ -38,7 +47,7 @@ void setup() {
   //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
   //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
     Serial.println("Could not find a valid BMP3 sensor, check wiring!");
-    while (1);
+    //while (1);
   }
 
   // Set up oversampling and filter initialization
@@ -52,7 +61,10 @@ void loop() {
   if (! bmp.performReading()) {
     Serial.println("Failed to perform reading :(");
     return;
+  }else{
+    Serial.println("Pressure read success");
   }
+  /**
   Serial.print("Temperature = ");
   Serial.print(bmp.temperature);
   Serial.println(" *C");
@@ -66,5 +78,14 @@ void loop() {
   Serial.println(" m");
 
   Serial.println();
-  delay(2000);
+  
+  */
+
+ if(tflI2C.getData(tfDist, tfAddr)){
+        Serial.println(String(tfDist)+" cm / " + String(tfDist/2.54)+" inches");
+  }else{
+    Serial.println("LiDAR read failed");
+  }
+
+  delay(200);
 }
