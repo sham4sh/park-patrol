6b1cf478c1c73637388ea9b8c21b468b70093b44
sham4sh <joshuashamash+1@gmail.com>
2024-04-24 17:49:55 -0400

    Bluetooth readout initial test

--- Park Patrol/src/main.cpp
+++ Park Patrol/src/main.cpp
@@ -18,7 +18,7 @@
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
-static const int RXPin = 0, TXPin = 1; 
+static const int RXPin = 3, TXPin = 2; 
 static const uint32_t GPSBaud = 9600;
 
 TinyGPSPlus gps; 
@@ -30,6 +30,8 @@ TFLI2C tflI2C;
  
 int16_t  tfDist;    // distance in centimeters
 int16_t  tfAddr = TFL_DEF_ADR;  // Use this default I2C address
+
+int state = 0;
 
 // Parking spaces occupied states - 0 free, 1 occupied
 int spaces[2][13] = 
@@ -76,7 +78,7 @@ std::pair<double, double> getCoordinates()
   std::pair<double, double> returnPair = {0, 0};
   if (gps.location.isValid()) { 
     returnPair.first = gps.location.lat(); 
-    returnPair.second = gps.location.lng();
+    returnPair.second = abs(gps.location.lng());
   }
 
   return returnPair;
@@ -109,7 +111,9 @@ if (gps.date.isValid()) {
 void setup() {
   Wire.begin();
 
-  Serial.begin(9600);
+  //Serial.begin(9600);
+
+  Serial.begin(9600); // Default communication rate of the Bluetooth module
 
   ss.begin(GPSBaud); 
   Serial.println(F("DeviceExample.ino")); // Printing information for demonstration and version of TinyGPSPlus library // by Mikal Hart 
@@ -131,41 +135,43 @@ void setup() {
   bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 
   // Mapped coordinates for every parking spot. The key corresponds to the spot's index in the occupied state matrix.
-  locations[{ 0, 0 }] = {1232.12, 1923.4}; 
-  locations[{ 0, 1 }] = {1232.12, 1923.4}; 
-  locations[{ 0, 2 }] = {1232.12, 1232.12}; 
-  locations[{ 0, 3 }] = {1232.12, 1923.4};
-  locations[{ 0, 4 }] = {1232.12, 1923.4}; 
-  locations[{ 0, 5 }] = {1232.12, 1923.4}; 
-  locations[{ 0, 6 }] = {1232.12, 1923.4}; 
-  locations[{ 0, 7 }] = {1232.12, 1923.4}; 
-  locations[{ 0, 8 }] = {1232.12, 1923.4}; 
-  locations[{ 0, 9 }] = {1232.12, 1232.12}; 
-  locations[{ 0, 10 }] = {1232.12, 1923.4};
-  locations[{ 0, 11 }] = {1232.12, 1923.4}; 
-  locations[{ 0, 12 }] = {1232.12, 1923.4}; 
+  locations[{ 0, 0 }] = {40.713340, 73.603134}; 
+  locations[{ 0, 1 }] = {40.713360, 73.603146}; 
+  locations[{ 0, 2 }] = {40.713380, 73.603158}; 
+  locations[{ 0, 3 }] = {40.713400, 73.603170};
+  locations[{ 0, 4 }] = {40.713420, 73.603182}; 
+  locations[{ 0, 5 }] = {40.713440, 73.603194}; 
+  locations[{ 0, 6 }] = {40.713460, 73.603206}; 
+  locations[{ 0, 7 }] = {40.713480, 73.603218}; 
+  locations[{ 0, 8 }] = {40.713500, 73.603240}; 
+  locations[{ 0, 9 }] = {40.713520, 73.603252}; 
+  locations[{ 0, 10 }] = {40.713540, 73.603264};
+  locations[{ 0, 11 }] = {40.713560, 73.603276}; 
+  locations[{ 0, 12 }] = {40.713580, 73.603288}; 
+  locations[{ 0, 13 }] = {40.713610, 73.603300}; 
 
-  locations[{ 1, 0 }] = {1232.12, 1923.4}; 
-  locations[{ 1, 1 }] = {1232.12, 1923.4}; 
-  locations[{ 1, 2 }] = {1232.12, 1232.12}; 
-  locations[{ 1, 3 }] = {1232.12, 1923.4};
-  locations[{ 1, 4 }] = {1232.12, 1923.4}; 
-  locations[{ 1, 5 }] = {1232.12, 1923.4}; 
-  locations[{ 1, 6 }] = {1232.12, 1923.4}; 
-  locations[{ 1, 7 }] = {1232.12, 1923.4}; 
-  locations[{ 1, 8 }] = {1232.12, 1923.4}; 
-  locations[{ 1, 9 }] = {1232.12, 1232.12}; 
-  locations[{ 1, 10 }] = {1232.12, 1923.4};
-  locations[{ 1, 11 }] = {1232.12, 1923.4}; 
-  locations[{ 1, 12 }] = {1232.12, 1923.4}; 
+  locations[{ 1, 0 }] = {40.713420, 73.603065}; 
+  locations[{ 1, 1 }] = {40.713435, 73.603076}; 
+  locations[{ 1, 2 }] = {40.713450, 73.603087}; 
+  locations[{ 1, 3 }] = {40.713465, 73.603098};
+  locations[{ 1, 4 }] = {40.713480, 73.603109}; 
+  locations[{ 1, 5 }] = {40.713495, 73.603120}; 
+  locations[{ 1, 6 }] = {40.713510, 73.603131}; 
+  locations[{ 1, 7 }] = {40.713525, 73.603142}; 
+  locations[{ 1, 8 }] = {40.713540, 73.603153}; 
+  locations[{ 1, 9 }] = {40.713555, 73.603164}; 
+  locations[{ 1, 10 }] = {40.713570, 73.603175};
+  locations[{ 1, 11 }] = {40.713585, 73.603186}; 
+  locations[{ 1, 12 }] = {40.713600, 73.603202}; 
+  locations[{ 1, 13 }] = {40.713634, 73.603218}; 
 }
 
 void loop() {
   if (! bmp.performReading()) {
-    Serial.println("Failed to perform reading :(");
+    //Serial.println("Failed to perform reading :(");
     return;
   }else{
-    Serial.println("Pressure read success");
+    //Serial.println("Pressure read success");
   }
   /**
   Serial.print("Temperature = ");
@@ -188,7 +194,9 @@ void loop() {
   Serial.print("Approx. Altitude = ");
   Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
   Serial.println(" m");
+  */
 
+  /**
  if(tflI2C.getData(tfDist, tfAddr)){
         Serial.println(String(tfDist)+" cm / " + String(tfDist/2.54)+" inches");
   }else{
@@ -197,37 +205,52 @@ void loop() {
 
   */  
 
-  if (gps.encode(ss.read())) { 
-    Serial.println("GPS found");
-    displayInfo(); 
-  }else{
-    Serial.println("GPS not found");
+  while (ss.available() > 0) 
+  { 
+    if (gps.encode(ss.read())) 
+    { 
+      displayInfo(); 
+    }
+  }
+  if (millis() > 5000 && gps.charsProcessed() < 10) 
+  { 
+    Serial.println(F("No GPS detected: check wiring.")); 
+    //while(true); 
   }
 
-  if (millis() > 5000 && gps.charsProcessed() < 10) { 
-    Serial.println(F("No GPS detected: check wiring.")); 
-  while(true); }
+  
 
 
+  
 
-  /**
 
   double altitude = bmp.readAltitude(SEALEVELPRESSURE_HPA);
   double distanceToGround = tfDist;
 
-  std::pair<double, double> coords = getCoordinates();
-  std::pair<int, int> closestSpace = findSpot(coords.first, coords.second);
+  if (gps.location.isValid()) 
+  { 
+    std::pair<double, double> coords = getCoordinates();
+    std::pair<int, int> closestSpace = findSpot(coords.first, coords.second);
 
-  Serial.print("Closest space: (");
-  Serial.print(closestSpace.first);
-  Serial.print(", ");
-  Serial.print(closestSpace.second);
-  Serial.println(")");
+    Serial.print("Closest space: (");
+    Serial.print(closestSpace.first);
+    Serial.print(", ");
+    Serial.print(closestSpace.second);
+    Serial.println(")");
 
-  Serial.print("Distance: ");
-  Serial.println(curDist);
+    Serial.print("Distance: ");
+    Serial.println(curDist);
+  }
 
-  */
+
+  Serial.write("Helloooooo");
+  
+
+  if(Serial.available() > 0){ // Checks whether data is comming from the serial port
+    Serial.println(Serial.read()); // Reads the data from the serial port
+  }
+
+  
 
 
   delay(200);
